8e649b537e1f53fab53d0ee27231faea1eea32e6
Henrry Alvarado <princedark393@gmail.com>
2023-11-18 16:57:35 -0500

    Tarea

---
+++ Programación en la nube/ConsumoSaaS/tagger_post.py
@@ -0,0 +1,58 @@
+import requests
+import json
+import shutil
+import os
+
+api_key = "acc_ebab055e18e88ba"
+api_secret = "665000c5cca4b044f8565bb48d0f6cd5"
+image_dir = "imagenes/"
+
+CLASSIFICATION_PATH = "imagenes/clasificacion/"
+CATEGORIES = [
+    "dog",
+    "cat",
+    "landscape",
+    "cars",
+    "food",
+    "streetview",
+    "beaches",
+    "art",
+]
+FILE_SEP = os.sep
+
+
+def checkPaths(categories, classification_path):
+    if not os.path.exists(classification_path):
+        os.mkdir(classification_path)
+    for category in categories:
+        target_path = classification_path + category + FILE_SEP
+        if not os.path.exists(target_path + category + FILE_SEP):
+            os.mkdir(target_path)
+
+
+def classifyImage(image_path, categories, classification_path):
+    response = requests.post(
+        "https://api.imagga.com/v2/tags",
+        auth=(api_key, api_secret),
+        files={"image": open(image_path, "rb")},
+    )
+    if response.status_code == 200:
+        data = response.json()
+        filename = image_path.split(FILE_SEP)[1]
+        for tag in data["result"]["tags"]:
+            for category in categories:
+                target_path = classification_path + FILE_SEP + category + FILE_SEP
+                if tag["confidence"] == 100 and tag["tag"]["en"] == category:
+                    shutil.copy(image_path, target_path + filename)
+    else:
+        print("API error " + image_path)
+
+
+checkPaths(CATEGORIES, CLASSIFICATION_PATH)
+
+for filename in os.listdir(image_dir):
+    f = os.path.join(image_dir, filename)
+    if os.path.isfile(f):
+        base_name, extension = os.path.splitext(filename)
+        image_path = os.path.join(image_dir, filename)
+        classifyImage(image_path, CATEGORIES, CLASSIFICATION_PATH)
